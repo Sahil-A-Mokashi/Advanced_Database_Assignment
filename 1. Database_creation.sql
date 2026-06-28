5b92c3ba79aa08c21b2ebde959ee187f18fa1638
Sahil Mokashi <101308350+Sahil-A-Mokashi@users.noreply.github.com>
2026-06-29 03:57:12 +0530

    Create PointPay database and set context
    This script creates the PointPay database and sets it for use.

---
+++ 1. Database_creation.sql
@@ -0,0 +1,12 @@
+-- =============================================
+-- PointPay Database Creation Script
+-- Advanced Databases - CA2
+-- =============================================
+
+-- Create the database
+CREATE DATABASE PointPay;
+GO
+
+-- Use the database
+USE PointPay;
+GO
